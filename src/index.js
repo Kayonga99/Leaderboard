ecda41242c77cafd76d7649797c2f8bfe66cc6d3
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-20 16:02:33 +0200

    Add api url.

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ const gameContainer = document.getElementById('form');
 const newScoreler = document.getElementById('name');
 const scores = document.getElementById('score');
 const listContainer = document.querySelector('.scores-list');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/infBnU0Sa9VBfIz7gMIc/scores/';
