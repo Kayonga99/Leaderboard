52fc6cb5533cca4e4ec7abfa0de7df23a5a6d161
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-20 16:05:03 +0200

    Add function.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,13 @@ const newScoreler = document.getElementById('name');
 const scores = document.getElementById('score');
 const listContainer = document.querySelector('.scores-list');
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/infBnU0Sa9VBfIz7gMIc/scores/';
+
+const renderScore = (gameUsers) => {
+    const gamers = (gameUsers.result);
+    let list = '';
+    gamers.forEach((gameUser) => {
+      list += `<li>${gameUser.user} : ${gameUser.score}</li>`;
+      listContainer.innerHTML = list;
+    });
+  };
+  
