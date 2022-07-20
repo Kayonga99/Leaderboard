f34917a458a43cf78bf1ece6dd12cb3884852047
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-20 15:46:30 +0200

    Add variables.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+const refresh = document.querySelector('.refresh');
+const gameContainer = document.getElementById('form');
+const newScoreler = document.getElementById('name');
+const scores = document.getElementById('score');
+const listContainer = document.querySelector('.scores-list');
