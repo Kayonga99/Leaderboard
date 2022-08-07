eb801de90239ea29de874746bf1d66315f9f6065
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-08-07 09:46:46 +0200

    Add new games url.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const gameContainer = document.getElementById('form');
 const newScoreler = document.getElementById('name');
 const scores = document.getElementById('score');
 const listContainer = document.querySelector('.scores-list');
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/infBnU0Sa9VBfIz7gMIc/scores/';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Xc9GMyeKmNsf4CJzvqNJ/scores/';
 
 const renderScore = (gameUsers) => {
   const gamers = (gameUsers.result);
